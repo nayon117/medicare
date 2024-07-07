7a18a50d196961236e00a134df87004e394b81b4
nayon117 <hasibul.nayon1@gmail.com>
2024-07-07 15:21:42 +0600

    implement consent part

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -2,14 +2,13 @@
 import RegisterForm from "@/components/forms/RegisterForm";
 import { getUser } from "@/lib/actions/patient.action";
 import Image from "next/image";
-import Link from "next/link";
 
 const Register = async({params:{userId}}:SearchParamProps) => {
    const user = await getUser(userId)
   return (
     <div className="flex h-screen min-h-screen">
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
+      <section className="remove-scrollbar container">
+        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
           <Image
             src="/assets/icons/logo-icon.svg"
             width={1000}
@@ -20,14 +19,9 @@ const Register = async({params:{userId}}:SearchParamProps) => {
 
           <RegisterForm user={user} />
 
-          <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">
+            <p className="copyright py-12">
               ©2024 Medicare
             </p>
-            <Link href="/?admin=true" className="text-green-500">
-              Admin
-            </Link>
-          </div>
         </div>
       </section>
       <Image
--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -19,6 +19,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select";
 import { Textarea } from "./ui/textarea";
+import { Checkbox } from "./ui/checkbox";
 
 //  CustomProps interface
 interface CustomProps {
@@ -136,6 +137,19 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
       );
     case FormFieldType.SKLETON:
       return renderSkeleton ? renderSkeleton(field) : null;
+    case FormFieldType.CHECKBOX:
+      return (
+        <FormControl>
+        <div className="flex items-center gap-4">
+          <Checkbox  
+          id={props.name}
+          checked={field.value}
+          onCheckedChange={field.onChange}
+          />
+          <label htmlFor={props.name} className="checkbox-label">{props.label}</label>
+        </div>
+      </FormControl>
+      )
     default:
       break;
   }
