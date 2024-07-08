75646febd10e8bdf65d2ec2d796f814a7a47ec8d
nayon117 <hasibul.nayon1@gmail.com>
2024-07-08 20:40:33 +0600

    integrate messaging system

--- lib/actions/appointment.action.ts
+++ lib/actions/appointment.action.ts
@@ -1,98 +1,130 @@
 /* eslint-disable no-undef */
-"use server"
+"use server";
 
 import { ID, Query } from "node-appwrite";
-import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config";
-import { parseStringify } from "../utils";
+import {
+  APPOINTMENT_COLLECTION_ID,
+  DATABASE_ID,
+  databases,
+  messaging,
+} from "../appwrite.config";
+import { formatDateTime, parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite.types";
 import { revalidatePath } from "next/cache";
 
-export const createAppointment = async (appointment:CreateAppointmentParams) =>{
-    try {
-        const newAppointment = await databases.createDocument(
-            DATABASE_ID!,
-            APPOINTMENT_COLLECTION_ID!,
-            ID.unique(),
-            appointment
-           )
-           return parseStringify(newAppointment)
-    } catch (error) {
-        console.log(error);
-    }
-}
+export const createAppointment = async (
+  appointment: CreateAppointmentParams
+) => {
+  try {
+    const newAppointment = await databases.createDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      ID.unique(),
+      appointment
+    );
+    return parseStringify(newAppointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const getAppointment = async (appointmentId:string) =>{
-    try {
-        const appointment = await databases.getDocument(
-            DATABASE_ID!,
-            APPOINTMENT_COLLECTION_ID!,
-            appointmentId
-           )
-           return parseStringify(appointment)
-    } catch (error) {
-        console.log(error);
-    }
-}
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const appointment = await databases.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+    return parseStringify(appointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const getRecentAppointmentList = async() =>{
-    try {
-        const appointments = await databases.listDocuments(
-            DATABASE_ID!,
-            APPOINTMENT_COLLECTION_ID!,
-            [Query.orderDesc('$createdAt')]
-        )
-        const initialCounts = {
-            scheduledCount : 0,
-            pendingCount : 0,
-            cancelledCount : 0
+export const getRecentAppointmentList = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc("$createdAt")]
+    );
+    const initialCounts = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    };
+
+    const counts = (appointments.documents as Appointment[]).reduce(
+      (acc, appointments) => {
+        if (appointments.status === "scheduled") {
+          acc.scheduledCount += 1;
+        } else if (appointments.status === "pending") {
+          acc.pendingCount += 1;
+        } else if (appointments.status === "cancelled") {
+          acc.cancelledCount += 1;
         }
+        return acc;
+      },
+      initialCounts
+    );
 
-        const counts =( appointments.documents as Appointment[]).reduce((acc,appointments)=>{
-            if(appointments.status === 'scheduled'){
-                acc.scheduledCount += 1
-            }
-            else if(appointments.status === 'pending'){
-                acc.pendingCount += 1
-            }
-           else if(appointments.status === 'cancelled'){
-                acc.cancelledCount += 1
-            }
-            return acc
-        
-        },initialCounts)
+    const data = {
+      totalCount: appointments.total,
+      ...counts,
+      documents: appointments.documents,
+    };
 
-        const data = {
-            totalCount:appointments.total,
-            ...counts,
-            documents:appointments.documents
-        }
+    return parseStringify(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-        return parseStringify(data)
+export const updateAppointment = async ({
+  appointmentId,
+  userId,
+  appointment,
+  type,
+}: UpdateAppointmentParams) => {
+  try {
+    const updatedAppointment = await databases.updateDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId,
+      appointment
+    );
 
-    } catch (error) {
-        console.log(error);
+    if (!updatedAppointment) {
+      throw new Error("Failed to update appointment");
     }
-}
 
-export const updateAppointment = async ({appointmentId,userId,appointment,type}:UpdateAppointmentParams) =>{
-    try {
-        const updatedAppointment = await databases.updateDocument(
-            DATABASE_ID!,
-            APPOINTMENT_COLLECTION_ID!,
-            appointmentId,
-            appointment
-        )
+    // sms/ notification
+    const smsMessage = `
+    Hi, it's Medicare. 
+    ${type === "schedule" 
+    ? `your appointment has been scheduled for ${formatDateTime(appointment.schedule!).dateTime} with Dr. ${appointment.primaryPhysician}` 
+    : `we regret to inform you that your appointment has been cancelled.Reason: ${appointment.cancellationReason}`}.
+    `;
 
-        if(!updatedAppointment){
-            throw new Error('Failed to update appointment')
-        }
+    await sendSMSNotification(userId, smsMessage);
 
-        // sms/ notification
+    revalidatePath("/admin");
+    return parseStringify(updatedAppointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-        revalidatePath('/admin')
-        return parseStringify(updatedAppointment)
-
-    } catch (error) {
-        console.log(error);
-    }
-}
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId]
+    );
+    return parseStringify(message);
+  } catch (error) {
+    console.log(error);
+  }
+};
